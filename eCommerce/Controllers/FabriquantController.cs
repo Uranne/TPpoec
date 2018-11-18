68697c75a897221e563028e2424238a7c6662e63
kevin <jimmy-the-otaku@hotmail.fr>
2018-11-18 23:09:11 +0100

    Création CRUD Produit + index et création Fabriquant

---
+++ eCommerce/Controllers/FabriquantController.cs
@@ -0,0 +1,97 @@
+﻿using eCommerce.DataAcccess;
+using eCommerce.DataAccess;
+using eCommerce.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class FabriquantController : Controller
+    {
+        IRepository<Fabriquant> RepFab = new EFRepository<Fabriquant>();
+
+        // GET: Fabriquant
+        //Version BackOffice de l'index
+        public ActionResult Index()
+        {
+            return View(RepFab.Lister());
+        }
+
+        // GET: Fabriquant/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Fabriquant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Fabriquant/Create
+        //La création est uniquement disponible pour le backoffice  
+        [HttpPost]
+        public ActionResult Create(Fabriquant f)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                RepFab.Ajouter(f);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Fabriquant/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Fabriquant/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Fabriquant/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Fabriquant/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- eCommerce/Controllers/ProduitController.cs
+++ eCommerce/Controllers/ProduitController.cs
@@ -9,26 +9,38 @@ using System.Web.Mvc;
 
 namespace eCommerce.Controllers
 {
+    // TODO: Penser à proposer une méthode de désactivation ou activation de produit multiple
+    // TODO: Proposer des méthodes de tri pour les Produits : Nom, Id, Status, Fabriquants
     public class ProduitController : Controller
     {
         IRepository<Produit> RepProduit = new EFRepository<Produit>();
+        IRepository<Fabriquant> RepFab = new EFRepository<Fabriquant>();
 
         // GET: Produit
+        //Index des produits côté BackOffice
         public ActionResult Index()
         {
             return View(RepProduit.Lister());
         }
 
         // GET: Produit/Details/5
+        //Détail des produits côté BackOffice
         public ActionResult Details(int id)
         {
-            return View();
+            return View(RepProduit.Trouver(id));
         }
 
         // GET: Produit/Create
+        //Création d'un produit : cette méthode ne peut être executé par un client
         public ActionResult Create()
         {
+            InitialisationDropListFabriquant();
             return View();
+        }
+
+        private void InitialisationDropListFabriquant()
+        {
+            ViewBag.IDFabriquants = RepFab.Lister().Select(f => new SelectListItem { Text = f.Nom, Value = f.Id.ToString() });
         }
 
         // POST: Produit/Create
@@ -37,9 +49,14 @@ namespace eCommerce.Controllers
         {
             try
             {
-                // TODO: Add insert logic here
+                // TODO: Ajouter la mise en place de photo
+                // TODO: Proposer une vue partielle pour créer le fabriquant dans cette vue
+                // TODO: Permettre de mettre des prix décimaux
+                // TODO: AJouter un bouton cloner en plus de edit, détails et delete qui prérempli le formulaire (Emmene vers une vue détail avec l'option de cloner en plus)
+                // TODO: Le catch de l'erreur
                 RepProduit.Ajouter(p);
                 return RedirectToAction("Index");
+                //Il serait aussi possible de renvoyer au détail de l'objet plutôt qu'à l'index en sortie de création d'objet
             }
             catch
             {
@@ -50,39 +67,18 @@ namespace eCommerce.Controllers
         // GET: Produit/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            InitialisationDropListFabriquant();
+            return View(RepProduit.Trouver(id));
         }
 
         // POST: Produit/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, Produit p)
         {
             try
             {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Produit/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Produit/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
+                // TODO: Le catch de l'erreur
+                RepProduit.Modifier(id, p);
                 return RedirectToAction("Index");
             }
             catch
